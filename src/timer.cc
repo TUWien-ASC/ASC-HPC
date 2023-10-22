cd77835cf173a330c32773f2dd557fed6bb84485
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2023-10-22 10:36:35 +0200

    added timer

---
+++ src/timer.cc
@@ -0,0 +1,206 @@
+#include <fstream>
+
+#include "timer.h"
+#include "taskmanager.h"
+
+
+namespace ASC_HPC
+{
+  thread_local std::unique_ptr<TimeLine> timeline;
+  std::mutex TimeLine::timeline_mutex;
+  std::mutex Timer::m;
+  std::vector<std::string> Timer::names;
+  std::vector<std::array<float,3>> Timer::cols;      
+  // int Timer::cnt = 0;
+
+
+  TimeLine :: TimeLine(std::string _filename)
+    : filename(_filename)
+  {
+    events.reserve(1000*1000);
+    start = GetTimeCounter();
+    start_time = std::chrono::high_resolution_clock::now();
+  }
+  
+  TimeLine :: ~TimeLine()
+  {
+    std::lock_guard<std::mutex> lock(output_mutex);
+    /*
+    if (subtl.size())
+      std::cout << "have sub-tls: " << subtl.size() << std::endl;
+    if (events.size())
+      Print (std::cout);
+    */
+    
+    if (filename != "")
+      {
+        auto end_time = std::chrono::high_resolution_clock::now();
+        auto end = GetTimeCounter();
+        auto duration = end_time-start_time;
+        
+        std::cout << "total time = "
+                  << std::chrono::duration_cast<std::chrono::microseconds>(duration).count()
+                  << " microsec" << std::endl;
+
+        double fac = 1e-3*double(std::chrono::duration_cast<std::chrono::microseconds>(duration).count()) / double(end-start);
+        
+        std::cout << "write pajefile '" << filename << "'" << std::endl;
+
+        /*
+          documentation of paje-format:
+          https://paje.sourceforge.net/download/publication/lang-paje.pdf
+         */
+        
+        std::ofstream file(filename);
+        file << R"(
+%EventDef PajeDefineContainerType 0 
+%       Alias string 
+%       Type string 
+%       Name string 
+%EndEventDef 
+%EventDef PajeDefineVariableType 1 
+%       Alias string 
+%       Type string 
+%       Name string 
+%       Color color 
+%EndEventDef 
+%EventDef PajeDefineStateType 2 
+%       Alias string 
+%       Type string 
+%       Name string 
+%EndEventDef 
+%EventDef PajeDefineEventType 3 
+%       Alias string 
+%       Type string 
+%       Name string 
+%       Color color 
+%EndEventDef 
+%EventDef PajeDefineLinkType 4 
+%       Alias string 
+%       Type string 
+%       StartContainerType string 
+%       EndContainerType string 
+%       Name string 
+%EndEventDef 
+%EventDef PajeDefineEntityValue 5 
+%       Alias string 
+%       Type string 
+%       Name string 
+%       Color color 
+%EndEventDef 
+%EventDef PajeCreateContainer 6 
+%       Time date 
+%       Alias string 
+%       Type string 
+%       Container string 
+%       Name string 
+%EndEventDef 
+%EventDef PajeDestroyContainer 7 
+%       Time date 
+%       Type string 
+%       Name string 
+%EndEventDef 
+%EventDef PajeSetVariable 8 
+%       Time date 
+%       Type string 
+%       Container string 
+%       Value double 
+%EndEventDef
+%EventDef PajeAddVariable 9 
+%       Time date 
+%       Type string 
+%       Container string 
+%       Value double 
+%EndEventDef
+%EventDef PajeSubVariable 10 
+%       Time date 
+%       Type string 
+%       Container string 
+%       Value double 
+%EndEventDef
+%EventDef PajeSetState 11 
+%       Time date 
+%       Type string 
+%       Container string 
+%       Value string 
+%EndEventDef
+%EventDef PajePushState 12 
+%       Time date 
+%       Type string 
+%       Container string 
+%       Value string 
+%       Id string 
+%EndEventDef
+%EventDef PajePopState 13 
+%       Time date 
+%       Type string 
+%       Container string 
+%EndEventDef
+%EventDef PajeResetState 14 
+%       Time date 
+%       Type string 
+%       Container string 
+%EndEventDef
+%EventDef PajeStartLink 15 
+%       Time date 
+%       Type string 
+%       Container string 
+%       Value string 
+%       StartContainer string 
+%       Key string 
+%EndEventDef
+%EventDef PajeEndLink 16 
+%       Time date 
+%       Type string 
+%       Container string 
+%       Value string 
+%       EndContainer string 
+%       Key string 
+%EndEventDef
+%EventDef PajeNewEvent 17 
+%       Time date 
+%       Type string 
+%       Container string 
+%       Value string 
+%EndEventDef
+
+
+0	main	0	"Task Manager"
+0	thds	main	"Thread"
+2	thdstate 	thds	"Task"
+6	0	a9	main	0	"Paje"
+)";
+
+        for (int i = 0; i <= subtl.size(); i++)
+          file << "6 0 th" << i << " thds a9 \"Thread " << i << "\"" << std::endl;
+
+        for (size_t i = 0; i < Timer::names.size(); i++)
+          {
+            auto col = Timer::cols[i];
+            file << "5 timer" << i << " thdstate \"" << Timer::names[i] << "\"  \"" << col[0] << " " << col[1] << " " << col[2] << "\"" << std::endl;
+          }
+        
+        for (int i = 0; i <= subtl.size(); i++)
+          {
+            auto * tl = (i==0) ? this : &subtl[i-1];
+            for (auto e : tl->events)
+              {
+                file << ((e.what==0) ? 12 : 13) << " ";
+                file << fac*(e.when-start) << " thdstate th" << i << " ";
+                if (e.what == 0)
+                  file << "timer" << e.timer << " idx ";
+                file << std::endl;
+              }
+          }
+      }
+  }
+
+  
+  
+  void TimeLine :: Print (std::ostream & ost) const
+  {
+    ost << "ending timeline:" << std::endl;
+    for (auto e : events)
+      ost << e.when-start << ", " << e.timer << ", " << e.what << std::endl;
+  }
+}
---
+++ src/timer.h
@@ -0,0 +1,149 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include<iostream>
+#include<string>
+#include<memory>
+
+
+
+
+
+#if defined(__APPLE__)
+#include <mach/mach_time.h>
+#endif
+
+#if defined(__amd64__) || defined(_M_AMD64)
+#ifdef WIN32
+#include <intrin.h>   // for __rdtsc()  CPU time step counter
+#else
+#include <x86intrin.h>   // for __rdtsc()  CPU time step counter
+#endif // WIN32
+#endif
+
+
+
+
+namespace ASC_HPC
+{
+
+
+  inline size_t GetTimeCounter() 
+  {
+#if defined(__APPLE__)
+    return mach_absolute_time();
+#endif
+    
+#if defined(__amd64__) || defined(_M_AMD64)
+    return __rdtsc();    
+#endif
+    
+    return std::chrono::high_resolution_clock::now().time_since_epoch().count();
+    
+    /*
+    // copied from ngcore/utils
+    #if defined(__APPLE__) && defined(NETGEN_ARCH_ARM64)
+    return mach_absolute_time();
+    #elif defined(NETGEN_ARCH_AMD64)
+    return __rdtsc();
+    #elif defined(NETGEN_ARCH_ARM64) && defined(__GNUC__)
+    // __GNUC__ is also defined by CLANG. Use inline asm to read Generic Timer
+    unsigned long long tics;
+    __asm __volatile("mrs %0, CNTVCT_EL0" : "=&r" (tics));
+    return tics;
+    #elif defined(__EMSCRIPTEN__)
+    return std::chrono::high_resolution_clock::now().time_since_epoch().count();
+    #else
+    #warning "Unsupported CPU architecture"
+    return 0;
+    #endif
+    */
+  }
+
+
+  struct Event
+  {
+    size_t when;
+    int timer;
+    int what; // 0..start, 1..stop
+  };
+
+
+  class TimeLine
+  {
+    size_t start;
+    std::chrono::time_point<std::chrono::high_resolution_clock> start_time;
+    std::vector<Event> events;
+    std::vector<TimeLine> subtl;
+    static std::mutex timeline_mutex;
+    std::string filename;
+  public:
+    TimeLine(std::string _filename = "");
+    TimeLine(const TimeLine&) = default;    
+    TimeLine(TimeLine&&) = default;
+    ~TimeLine();
+    
+    void Add (Event event)
+    {
+      events.push_back(event);
+    }
+
+    void AddTimeLine (TimeLine sub)
+    {
+      std::lock_guard<std::mutex> lock(timeline_mutex);      
+      subtl.push_back(std::move(sub));
+    }
+
+    void Print (std::ostream & ost) const;
+  };
+  
+  extern thread_local std::unique_ptr<TimeLine> timeline;
+
+  
+  class Timer
+  {
+    int nr;
+    static std::vector<std::string> names;
+    static std::vector<std::array<float,3>> cols;    
+    static std::mutex m;
+  public:     
+    Timer(const std::string & name, std::array<float,3> col = { 0, 1, 0})
+    {
+      std::lock_guard<std::mutex> lock(m);            
+      nr = names.size();
+      names.push_back(name);
+      cols.push_back(col);
+    }
+
+    void Start()
+    {
+      if (timeline)
+        timeline->Add (Event{GetTimeCounter(), nr, 0});
+    }
+
+    void Stop()
+    {
+      if (timeline)
+        timeline->Add(Event{GetTimeCounter(), nr, 1});
+    }
+    friend TimeLine;
+  };
+
+
+  class RegionTimer
+  {
+    Timer & t;
+  public:
+    RegionTimer (Timer & _t) : t(_t)
+    {
+      t.Start();
+    }
+    ~RegionTimer ()
+    {
+      t.Stop();
+    }
+  };
+
+}
+
+#endif
