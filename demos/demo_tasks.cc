aa1ddcf8f3f6a52e69ee95bb558fc883d065ab3c
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2023-10-22 17:16:47 +0200

    work on demos

--- demos/demo_tasks.cc
+++ demos/demo_tasks.cc
@@ -13,20 +13,14 @@ int main()
 
   StartWorkers(3);
 
-  RunParallel(100,  [] (size_t i, size_t size)
+  RunParallel(10,  [] (size_t i, size_t size)
   {
     static Timer t("timer one");
     RegionTimer reg(t);
     cout << "I am task " << i << " out of " << size << endl;
   });
 
-  RunParallel(100,  [] (size_t i, size_t size)
-  {
-    static Timer t("timer two", { 0, 0, 1});
-    RegionTimer reg(t);
-  });
-
-
+  
   RunParallel(6, [] (size_t i, size_t s)
   {
     RunParallel(6, [i](size_t j, size_t s2)
@@ -36,6 +30,33 @@ int main()
         cout << str.str();
     });
   });
+
+
+
+  
+  RunParallel(100,  [] (size_t i, size_t size)
+  {
+    static Timer t("timer two", { 0, 0, 1});
+    extern int myvar; myvar++;
+    RegionTimer reg(t);
+  });
+
+  RunParallel(1000,  [] (size_t i, size_t size)
+  {
+    static Timer t("timer 3", { 1, 0, 0});
+    RegionTimer reg(t);
+  });
+
+  RunParallel(100, [] (size_t i, size_t s)
+  {
+    static Timer t("timer 4", { 1, 1, 0});
+    RegionTimer reg(t);    
+    RunParallel(100, [i](size_t j, size_t s2)
+    {
+      ;
+    });
+  });
+
 
   
   StopWorkers();
