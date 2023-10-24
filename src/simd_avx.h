d9ccd531dc4d07837b955f97a77649cc2f6c943e
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2023-10-24 22:10:52 +0200

    mask(256d), first of IndexSequence has type T

--- src/simd_avx.h
+++ src/simd_avx.h
@@ -41,6 +41,35 @@ namespace ASC_HPC
     double operator[](size_t i) const { return val[i]; }
   };
   
+
+
+  
+  template<>
+  class SIMD<int64_t,4>
+  {
+    __m256i val;
+  public:
+    SIMD () = default;
+    SIMD (const SIMD &) = default;
+    // SIMD(double _val) : val{_mm256_set1_pd(_val)} {};
+    SIMD(__m256i _val) : val{_val} {};
+    SIMD (int64_t v0, int64_t v1, int64_t v2, int64_t v3) : val{_mm256_set_epi64x(v3,v2,v1,v0) } { } 
+    SIMD (SIMD<int64_t,2> v0, SIMD<int64_t,2> v1) : SIMD(v0[0], v0[1], v1[0], v1[1]) { }  // can do better !
+    // SIMD (std::array<double,4> a) : SIMD(a[0],a[1],a[2],a[3]) { }
+    // SIMD (double const * p) { val = _mm256_loadu_pd(p); }
+    // SIMD (double const * p, SIMD<mask64,4> mask) { val = _mm256_maskload_pd(p, mask.Val()); }
+    
+    static constexpr int Size() { return 4; }
+    auto Val() const { return val; }
+    // const double * Ptr() const { return (double*)&val; }
+    // SIMD<double, 2> Lo() const { return _mm256_extractf128_pd(val, 0); }
+    // SIMD<double, 2> Hi() const { return _mm256_extractf128_pd(val, 1); }
+    int64_t operator[](size_t i) const { return val[i]; }
+  };
+  
+
+
+
   
   inline auto operator+ (SIMD<double,4> a, SIMD<double,4> b) { return SIMD<double,4> (_mm256_add_pd(a.Val(), b.Val())); }
   inline auto operator- (SIMD<double,4> a, SIMD<double,4> b) { return SIMD<double,4> (_mm256_sub_pd(a.Val(), b.Val())); }
