984ea228d5f2dfc73d5aa597b7e4214baa602476
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2025-11-04 16:13:45 +0100

    m_val in avx-branch

--- src/simd_avx.hpp
+++ src/simd_avx.hpp
@@ -46,14 +46,14 @@ namespace ASC_HPC
     SIMD (double const * p, SIMD<mask64,4> mask) { m_val = _mm256_maskload_pd(p, mask.val()); }
     
     static constexpr int size() { return 4; }
-    auto val() const { return val; }
-    const double * ptr() const { return (double*)&val; }
+    auto val() const { return m_val; }
+    const double * ptr() const { return (double*)&m_val; }
     SIMD<double, 2> lo() const { return SIMD<double,2>((*this)[0], (*this)[1]); }
     SIMD<double, 2> hi() const { return SIMD<double,2>((*this)[2], (*this)[3]); }
 
     // better:
     // SIMD<double, 2> lo() const { return _mm256_extractf128_pd(m_val, 0); }
-    // SIMD<double, 2> hi() const { return _mm256_extractf128_pd(m)val, 1); }
+    // SIMD<double, 2> hi() const { return _mm256_extractf128_pd(m_val, 1); }
     double operator[](size_t i) const { return ((double*)&m_val)[i]; }
 
     void store (double * p) const { _mm256_storeu_pd(p, m_val); }
@@ -66,24 +66,24 @@ namespace ASC_HPC
   template<>
   class SIMD<int64_t,4>
   {
-    __m256i val;
+    __m256i m_val;
   public:
     SIMD () = default;
     SIMD (const SIMD &) = default;
-    SIMD(int64_t _val) : val{_mm256_set1_epi64x(_val)} {};
-    SIMD(__m256i _val) : val{_val} {};
-    SIMD (int64_t v0, int64_t v1, int64_t v2, int64_t v3) : val{_mm256_set_epi64x(v3,v2,v1,v0) } { } 
+    SIMD(int64_t val) : m_val{_mm256_set1_epi64x(_val)} {};
+    SIMD(__m256i val) : m_val{val} {};
+    SIMD (int64_t v0, int64_t v1, int64_t v2, int64_t v3) : m_val{_mm256_set_epi64x(v3,v2,v1,v0) } { } 
     SIMD (SIMD<int64_t,2> v0, SIMD<int64_t,2> v1) : SIMD(v0[0], v0[1], v1[0], v1[1]) { }  // can do better !
     // SIMD (std::array<double,4> a) : SIMD(a[0],a[1],a[2],a[3]) { }
     // SIMD (double const * p) { val = _mm256_loadu_pd(p); }
     // SIMD (double const * p, SIMD<mask64,4> mask) { val = _mm256_maskload_pd(p, mask.val()); }
     
     static constexpr int size() { return 4; }
-    auto val() const { return val; }
+    auto val() const { return m_val; }
     // const double * Ptr() const { return (double*)&val; }
     // SIMD<double, 2> Lo() const { return _mm256_extractf128_pd(val, 0); }
     // SIMD<double, 2> Hi() const { return _mm256_extractf128_pd(val, 1); }
-    int64_t operator[](size_t i) const { return ((int64_t*)&val)[i]; }
+    int64_t operator[](size_t i) const { return ((int64_t*)&m_val)[i]; }
   };
   
 
